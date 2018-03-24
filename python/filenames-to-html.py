1f19ce48323dd0d8b08223a7d68198a7bae97fa6
Katherine Donnally <kbd3ax@virginia.edu>
2018-03-24 17:24:08 -0400

    Add & call fxns originally defined in html-to-classes.py

--- python/filenames-to-html.py
+++ python/filenames-to-html.py
@@ -1,2 +1,22 @@
 # filenames-to-html.py
 # match images w/ project titles
+
+# FUNCTIONS:
+
+# using regex to search and grab
+def regexTextSearch(file, pattern):
+	import re
+
+	with open(file, 'rt') as myfile:
+		matches = re.findall(pattern, myfile.read())
+		return matches
+
+# CALLS:
+
+# match string s where s == 'class="<val>"'
+pattern = r'class=".*?"'
+
+# list of all 'class="<val>"' in file
+matches = regexTextSearch('research.html', pattern)
+
+print(matches)
