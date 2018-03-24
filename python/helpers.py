e45c50d6ebae79a1fd3229397a58af7aa0dd22e1
Katherine Donnally <kbd3ax@virginia.edu>
2018-03-24 17:26:11 -0400

    Add regex text search fxn to helpers.py

--- python/helpers.py
+++ python/helpers.py
@@ -1,2 +1,14 @@
 # helpers.py
 # reusable functions for web dev
+
+# using regex to search and grab
+def regexTextSearch(file, pattern):
+	import re
+
+	with open(file, 'rt') as myfile:
+		matches = re.findall(pattern, myfile.read())
+		return matches
+
+# example pattern:
+# match string s where s == 'class="<val>"'
+pattern = r'class=".*?"'
