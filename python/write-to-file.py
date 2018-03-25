699f5fa35b9c2bd87b10b553db96f052ede3d836
Katherine Donnally <kbd3ax@virginia.edu>
2018-03-24 20:47:52 -0400

    Create Python script that takes a list of files & strings -> appends to each

---
+++ python/write-to-file.py
@@ -0,0 +1,26 @@
+# create or open file & write to it
+
+def writeToFile(file, list):
+	with open(file, 'at') as myfile:
+		for l in list:
+			myfile.write(l + '\n')
+
+el = ['one', 'two', 'three']
+files = ['test1', 'test2', 'test3']
+
+'''def testFile(files):
+	for file in files:
+		with open(file + '.md', 'wt') as myfile:
+			myfile.write('will this get erased?\n')
+'''
+# testFile(files)
+
+# create & write, or append if exists
+# files = list of filenames
+# add = list of strings to append
+def addToFiles(files, add):
+	for file, a in zip(files, add):
+		with open(file + '.md', 'at') as myfile:
+			myfile.write(a)
+
+# addToFiles(files, el)
