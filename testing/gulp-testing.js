9c6509adbefd9278459386040ccd8223c9f7560b
Katherine Donnally <kbd3ax@virginia.edu>
2018-06-07 13:34:05 -0400

    Move old gulpfile to a testing file to preserve contents

---
+++ testing/gulp-testing.js
@@ -0,0 +1,23 @@
+// 1st pass at a gulpfile
+
+// fetch module
+var gulp 	 = require('gulp'),
+	concat 	 = require('gulp-concat'),
+	prefixer = require('gulp-autoprefixer');
+
+// files to read, where to write them
+var testFiles = './assets/css/_sass/*.scss',
+	outputDir = './testing/gulp-output/concat';
+
+// example: put all scss in 1 file
+gulp.task('concat-scss', () => {
+	return gulp.src(testFiles)
+		.pipe(concat('style.scss'))
+		.pipe(gulp.dest(outputDir));
+});
+
+var watch = gulp.task('watch', () => {
+	return gulp.watch(testFiles, ['concat-scss']);
+})
+
+gulp.task('default', gulp.parallel('concat-scss', 'watch'));
