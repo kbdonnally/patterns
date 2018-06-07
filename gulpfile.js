7d41f79874afdb8b1e80c9e8311e2b871fdcf33c
Katherine Donnally <kbd3ax@virginia.edu>
2018-06-07 11:55:11 -0400

    Update gulpfile & json packages

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,21 @@
 // fetch module
-var gulp = require('gulp');
+var gulp 	 = require('gulp'),
+	concat 	 = require('gulp-concat'),
+	prefixer = require('gulp-autoprefixer');
 
-gulp.task('default', function() {
-	// whatever you want to happen when
-	// you run 'gulp' in cmd
+// files to read, where to write them
+var testFiles = './assets/css/_sass/*.scss',
+	outputDir = './testing/gulp-output/concat';
+
+// example: put all scss in 1 file
+gulp.task('concat-scss', () => {
+	return gulp.src(testFiles)
+		.pipe(concat('style.scss'))
+		.pipe(gulp.dest(outputDir));
 });
+
+var watch = gulp.task('watch', () => {
+	return gulp.watch(testFiles, ['concat-scss']);
+})
+
+gulp.task('default', gulp.parallel('concat-scss', 'watch'));
