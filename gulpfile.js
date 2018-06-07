bd3b952c6c73f8f5091eaa11f549e1d9469f1934
Katherine Donnally <kbd3ax@virginia.edu>
2018-06-07 17:38:45 -0400

    Get auto-prefixer working, at least manually!

--- gulpfile.js
+++ gulpfile.js
@@ -12,9 +12,13 @@ var autoprefixer = require('gulp-autoprefixer'),
 
 // tasks:
 
+// js:
+
+// set file locations
 var src_js = './assets/js-new/_js/*.js';
 var dest_js = './assets/js-new';
 
+// minify & combine files
 var format_js = () => {
 	return gulp.src(src_js)
 		.pipe(concat('main.js'))
@@ -24,4 +28,27 @@ var format_js = () => {
 
 gulp.task('watch_js', () => {
 	return gulp.watch(src_js, format_js);
+});
+
+gulp.task('clean_js', () => {
+	return gulp.src('assets/js-new/main.js', { allowEmpty: true })
+		.pipe(clean());
+});
+
+gulp.task('js', gulp.series('clean_js', 'watch_js'));
+
+// css:
+
+
+gulp.task('format_css', () => {
+	return gulp.src('_site/assets/css/style.css')
+		.pipe(autoprefixer({
+			browsers: ['last 2 versions'],
+			grid: true
+		}))
+		.pipe(gulp.dest('testing/gulp-output/css'));
+});
+
+gulp.task('watch_css', () => {
+	return gulp.watch('_site/assets/css/style.css', format_css);
 });
